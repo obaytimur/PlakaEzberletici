7d9e5263598dbcae1856165252018bcab659fff7
Oğulcan Baytimur <baytimurogulcan@gmail.com>
2023-11-13 23:52:13 +0300

    feat: add city shower

--- PlakaEzberletici/PlakaEzberletici/ContentView.swift
+++ PlakaEzberletici/PlakaEzberletici/ContentView.swift
@@ -10,19 +10,40 @@ import SwiftUI
 struct ContentView: View {
     @State private var randomInt = Int.random(in: 10..<100)
     @State private var size = 300.0
+    @State private var tapPressed = false
     var body: some View {
         VStack {
             Spacer()
-            Text("\(randomInt)")
-                .font(.custom("FontNameMono", fixedSize: 200))
-                .fixedSize(horizontal: false, vertical: true)
-                .multilineTextAlignment(.center)
-                .padding()
-                .frame(width: 300, height: 200)
-                .background(Rectangle().fill(Color.white).shadow(radius: 3))
+            if let cityName = plakalar[String(randomInt)]{
+                if tapPressed {
+                    Text("\(cityName)")
+                        .font(.custom("FontNameMono", fixedSize: 70))
+                        .fixedSize(horizontal: false, vertical: true)
+                        .multilineTextAlignment(.center)
+                        .padding()
+                        .frame(width: 300, height: 200)
+                        .background(Rectangle().fill(Color.white).shadow(radius: 3))
+                        .onTapGesture {
+                            tapPressed = false
+                        }
+                }
+                else{
+                    Text("\(randomInt)")
+                        .font(.custom("FontNameMono", fixedSize: 200))
+                        .fixedSize(horizontal: false, vertical: true)
+                        .multilineTextAlignment(.center)
+                        .padding()
+                        .frame(width: 300, height: 200)
+                        .background(Rectangle().fill(Color.white).shadow(radius: 3))
+                        .onTapGesture {
+                            tapPressed = true
+                        }
+                }
+            }
             Spacer()
             Button ("New Number") {
                 randomInt = Int.random(in: 10..<100)
+                tapPressed = false
             }
         }
         .padding()

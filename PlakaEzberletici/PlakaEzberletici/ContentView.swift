63bcf59b3d28acefd3b9b478a77794f5cb94ea24
Oğulcan Baytimur <baytimurogulcan@gmail.com>
2023-11-17 22:51:16 +0300

    feat: add toggles for light mode and number/name selections

--- PlakaEzberletici/PlakaEzberletici/ContentView.swift
+++ PlakaEzberletici/PlakaEzberletici/ContentView.swift
@@ -23,21 +23,66 @@ struct ContentView: View {
     @State private var size = 300.0
     @State private var tapPressed = false
     @State private var isDarkMode = false
+    @State private var nameToggle = false
     @Environment(\.colorScheme) private var colorScheme
     var body: some View {
         VStack {
+            HStack{
+                HStack{
+                    Image(systemName: "moon")
+                    Toggle("", isOn: $isDarkMode).labelsHidden()
+                    Image(systemName: "sun.min")
+                }
+                Spacer()
+                HStack{
+                    Image(systemName: "textformat.123")
+                    Toggle("", isOn: $nameToggle).labelsHidden()
+                    Image(systemName: "textformat")
+                }
+            }
             Spacer()
             if let cityName = plakalar[String(randomInt)]{
+                if nameToggle{
                     VStack{
-                        Text("\(randomInt)")
-                            .font(.custom("ArialRoundedMTBold", fixedSize: 150))
+                        Text("\(cityName)")
+                            .font(.custom("ArialRoundedMTBold", fixedSize: 50))
                             .fixedSize(horizontal: false, vertical: true)
-                            .foregroundStyle(colorScheme == .dark ? Color(hex: 0x354FAB) : Color(hex: 0xD7DCEE))
+                            .foregroundStyle(isDarkMode != true ? Color(hex: 0x354FAB) : Color(hex: 0xD7DCEE))
                             .multilineTextAlignment(.center)
                             .padding()
                             .frame(width: 250, height: 250)
                             .clipShape(RoundedRectangle(cornerRadius: 10))
-                            .background(colorScheme == .dark ? Color(hex: 0xD7DCEE) : Color(hex: 0x354FAB))
+                            .background(isDarkMode != true ? Color(hex: 0xD7DCEE) : Color(hex: 0x354FAB))
+                            .cornerRadius(15)
+                            .onTapGesture {
+                                tapPressed = !tapPressed
+                            }
+                            .overlay(
+                                Spacer()
+                                    .frame(height: 100)
+                            )
+                            Text("\(randomInt)")
+                                .opacity(tapPressed ? 1 : 0)
+                                .font(.custom("ArialRoundedMTBold", fixedSize: 50))
+                                .fixedSize(horizontal: false, vertical: true)
+                                .foregroundStyle(isDarkMode != true ? Color(hex: 0xD7DCEE) : Color(hex: 0x354FAB))
+                                .multilineTextAlignment(.center)
+                                .onTapGesture {
+                                    tapPressed = false
+                                }
+                    }
+                }
+                else{
+                    VStack{
+                        Text("\(randomInt)")
+                            .font(.custom("ArialRoundedMTBold", fixedSize: 150))
+                            .fixedSize(horizontal: false, vertical: true)
+                            .foregroundStyle(isDarkMode != true ? Color(hex: 0x354FAB) : Color(hex: 0xD7DCEE))
+                            .multilineTextAlignment(.center)
+                            .padding()
+                            .frame(width: 250, height: 250)
+                            .clipShape(RoundedRectangle(cornerRadius: 10))
+                            .background(isDarkMode != true ? Color(hex: 0xD7DCEE) : Color(hex: 0x354FAB))
                             .cornerRadius(15)
                             .onTapGesture {
                                 tapPressed = !tapPressed
@@ -50,21 +95,23 @@ struct ContentView: View {
                                 .opacity(tapPressed ? 1 : 0)
                                 .font(.custom("ArialRoundedMTBold", fixedSize: 50))
                                 .fixedSize(horizontal: false, vertical: true)
-                                .foregroundStyle(colorScheme == .dark ? Color(hex: 0xD7DCEE) : Color(hex: 0x354FAB))
+                                .foregroundStyle(isDarkMode != true ? Color(hex: 0xD7DCEE) : Color(hex: 0x354FAB))
                                 .multilineTextAlignment(.center)
                                 .onTapGesture {
                                     tapPressed = false
                                 }
                     }
+                }
             }
             Spacer()
-            Button ("New Number") {
+            Button ("New City") {
                 randomInt = Int.random(in: 10..<100)
                 tapPressed = false
             }
+            .foregroundStyle(isDarkMode != true ? Color(hex: 0xD7DCEE) : Color(hex: 0x354FAB))
         }
         .frame(maxWidth: .infinity)
-        .background(colorScheme == .dark ? Color(hex: 0x354FAB) : Color(hex: 0xD7DCEE))
+        .background(isDarkMode != true ? Color(hex: 0x354FAB) : Color(hex: 0xD7DCEE))
     }
 }
 
